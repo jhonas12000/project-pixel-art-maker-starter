65b83b13496a78281baa15021c62e6065e0a729d
jhonas12000 <ytwmichael@gmail.com>
2022-04-19 13:35:17 -0700

    javaSript added and worked properly

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+    const height = document.getElementById('inputHeight').value;
+    const width = document.getElementById('inputWidth').value;
+    const table = document.getElementById('pixelCanvas');
+    
+    let grid  = '';
+    for (let i = 0; i < height; i++) {
+        grid +="<tr>";
 
+            for (let j = 0; j < width; j++) {
+                grid += "<td></td>"
+            }
+        grid +="</tr>";
+    }
+    table.innerHTML = grid;
+    return false;
 }
+
+document.body.addEventListener('click', function(e) {
+    let target = e.target
+    let tag = target.tagName
+
+    let color = document.getElementById('colorPicker').value
+
+    if (tag == 'TD') {
+        target.style.background = color
+    }
+})
